74fa8fc2fbdfd7434b403938742d8d9e1573e1a2
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-02 23:53:50 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- M4/p3/longest_substring.py
+++ M4/p3/longest_substring.py
@@ -1,15 +1,15 @@
 '''Assume s is a string of lower case characters.'''
-str_s = input()
-m_c = 0
-g_c = 0
-c_co = 0
-for i in range(len(str_s)-1):
-    if str_s[i] <= str_s[i+1]:
-    	c_co += 1
+STR_S = input()
+M_C = 0
+G_C = 0
+C_CO = 0
+for i in range(len(STR_S)-1):
+    if STR_S[i] <= STR_S[i+1]:
+        C_CO += 1
     else:
-    	c_co = 0
-    if g_c < c_co:
-    	g_c = c_co
-    	m_c = i
-    d_d = m_c-g_c+1
-print(str_s[d_d:d_d+g_c+1])
+        C_CO = 0
+    if G_C < C_CO:
+        G_C = C_CO
+        M_C = i
+    d_d = M_C-G_C+1
+print(STR_S[d_d:d_d+G_C+1])
