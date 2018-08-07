ffd520e6681363103721178d9e915f3c34db4453
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-07 19:40:15 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- M8/p1/assignment1.py
+++ M8/p1/assignment1.py
@@ -6,19 +6,19 @@ This function takes in one number and returns one number.
 '''
 
 def factorial(num_ber):
-   '''
-   n is positive Integer
+    '''
+    n is positive Integer
 
-   returns: a positive integer, the factorial of n.
-   '''
-   if num_ber == 0:
-       return 1
-   return num_ber* factorial(num_ber-1)
+    returns: a positive integer, the factorial of n.
+    '''
+    if num_ber == 0:
+        return 1
+    return num_ber* factorial(num_ber-1)
 
 def main():
-   """defining main function"""
-   in_put = input()
-   print(factorial(int(in_put)))
+    """defining main function"""
+    in_put = input()
+    print(factorial(int(in_put)))
 
 if __name__ == "__main__":
-   main()
+    main()
