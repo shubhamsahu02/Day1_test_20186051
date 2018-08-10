08dc053600951b27e1fbbc989449ba37cf06f8b4
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 01:33:25 +0530

    " -> 9 of 9 passed. pylint: 7.65/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -9,10 +9,10 @@ def get_word_score(word, n_n):
 The score for a word is the sum of the points for letters in the
     word, multiplied by the length of the word, PLUS 50 points if all n
     letters are used on the first turn.
-    
+
 Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
     worth 3, D is worth 2, E is worth 1, and so on (see scrabble_values)
-    
+
 word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
