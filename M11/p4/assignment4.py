7aa88ebc400834dc8330b9eb63b6562c690cc320
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 00:47:17 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- M11/p4/assignment4.py
+++ M11/p4/assignment4.py
@@ -2,7 +2,6 @@
 #We are now ready to begin writing the code that interacts with the player.
 #We'll be implementing the playHand function.
 #This function allows the user to play out a single hand.
-"""First, though, you'll need to implement the helper calculateHandlen function, which can be done in under five lines of code."""
 
 
 def calculate_hand_len(hand):
