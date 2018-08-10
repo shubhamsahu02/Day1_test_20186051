880a27b2111a9dba27adc7bb1e39466e6f3ce725
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 00:11:48 +0530

    " -> 6 of 6 passed. pylint: 9.38/10 "

--- M11/p2/assignment2.py
+++ M11/p2/assignment2.py
@@ -1,5 +1,8 @@
 """Exercise: Assignment-2"""
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+#Implement the updateHand function.
+#Make sure this function has no side effects: i.e.,
+# it must not mutate the hand passed in. Before pasting your function definition here,
+#be sure you've passed the appropriate tests in test_ps4a.py.
 
 
 def u_h(hand, word):
@@ -7,22 +10,22 @@ def u_h(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
 
-    Has no side effects: does not modify hand.
+    Has no side effects: does notmodify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int) 
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    
+      # TO DO ... <-- Remove this comment when you code this function
+
     for char in word:
-      if char in hand:
-          hand[char] = hand[char]-1
+        if char in hand:
+            hand[char] = hand[char]-1
     return hand
 
 def main():
