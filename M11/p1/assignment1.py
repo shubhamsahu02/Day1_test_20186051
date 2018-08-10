38dea9c24addadf037bb2d73bfff34fd82a9c94d
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 01:33:05 +0530

    " -> 9 of 9 passed. pylint: 6.47/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -1,20 +1,19 @@
 '''
 Exercise: Assignment-1
-The first step is to implement some code that allows us to calculate the score for a single word. The function get_word_score should accept as input a string of lowercase letters (a word) and return the integer score for that word, using the game's scoring rules.
 '''
 
 def get_word_score(word, n_n):
     """
     Returns the score for a word. Assumes the word is a valid word.
     
-    The score for a word is the sum of the points for letters in the
+The score for a word is the sum of the points for letters in the
     word, multiplied by the length of the word, PLUS 50 points if all n
     letters are used on the first turn.
     
-    Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
+Letters are scored as in Scrabble; A is worth 1, B is worth 3, C is
     worth 3, D is worth 2, E is worth 1, and so on (see scrabble_values)
     
-    word: string (lowercase letters)
+word: string (lowercase letters)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     returns: int >= 0
     """
