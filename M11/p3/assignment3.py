3b796701ee4f0c663f476c36ea044d6504eb7cad
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 00:23:43 +0530

    " -> 3 of 3 passed. pylint: -6.67/10 "

--- M11/p3/assignment3.py
+++ M11/p3/assignment3.py
@@ -7,7 +7,7 @@ Testing: Make sure the test_isValidWord tests pass. In addition, you will want t
 Fill in the code for isValidWord in ps4a.py and be sure you've passed the appropriate tests in test_ps4a.py before pasting your function definition here.
 '''
 
-def isValidWord(word, hand, wordList):
+def isValidWord(word, hand, word_list):
     """
     Returns True if word is in the wordList and is entirely
     composed of letters in the hand. Otherwise, returns False.
