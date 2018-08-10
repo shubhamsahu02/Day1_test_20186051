ceccc90554f4841166f2a04f91d5f90772df8639
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 01:34:07 +0530

    " -> 9 of 9 passed. pylint: 7.65/10 "

--- M11/p1/assignment1.py
+++ M11/p1/assignment1.py
@@ -5,7 +5,7 @@ Exercise: Assignment-1
 def get_word_score(word, n_n):
     """
     Returns the score for a word. Assumes the word is a valid word.
-    
+ 
 The score for a word is the sum of the points for letters in the
     word, multiplied by the length of the word, PLUS 50 points if all n
     letters are used on the first turn.
@@ -18,9 +18,9 @@ word: string (lowercase letters)
     returns: int >= 0
     """
     scrabble_values = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
+     'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
     }
     sum_0 = 0
     l_l = len(word)
