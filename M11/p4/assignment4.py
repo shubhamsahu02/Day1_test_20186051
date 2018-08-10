de7352717f2d9ff5362f55ddb1724f7b9357ed19
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-11 00:46:29 +0530

    " -> 5 of 5 passed. pylint: 9.33/10 "

--- M11/p4/assignment4.py
+++ M11/p4/assignment4.py
@@ -1,4 +1,4 @@
-#Exercise: Assignment-4
+"""Exercise: Assignment-4"""
 #We are now ready to begin writing the code that interacts with the player.
 #We'll be implementing the playHand function.
 #This function allows the user to play out a single hand.
