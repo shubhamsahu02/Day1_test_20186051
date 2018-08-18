a0e304baafa6e9bedfa40c87f2ecd22470023bca
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-18 12:45:19 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

---
+++ M18/p1/build_search_index.py
@@ -0,0 +1,114 @@
+'''
+    Tiny Search Engine - Part 1 - Build a search index
+
+    In this programming assingment you are given with some text documents as input.
+    Complete the program below to build a search index. Don't worry, it is explained below.
+    A search index is a python dictionary.
+    The keys of this dictionary are words contained in ALL the input text documents.
+    The values are a list of documents such that the key/word appears in each document atleast once.
+    The document in the list is represented as a tuple.
+    The tuple has 2 items. The first item is the document ID.
+    Document ID is represented by the list index.
+    For example: the document ID of the third document in the list is 2
+    The second item of the tuple is the frequency of the word occuring in the document.
+
+    Here is the sample format of the dictionary.
+    {
+        word1: [(doc_id, frequency),(doc_id, frequency),...],
+        word2: [(doc_id, frequency),(doc_id, frequency),...],
+        .
+        .
+    }
+'''
+import re
+
+def del_stopwords(docs):
+
+# helper function to load the stop words from a file
+s_words = load_stopwords('stopwords.txt')
+for s_word in s_words:
+    if s_word in docs:
+        del docs[s_word]
+    return docs
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as f_stopwords:
+        for line in f_stopwords:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+
+def word_list(text):
+    '''
+        Change case to lower and split the words using a SPACE
+        Clean up the text by remvoing all the non alphabet characters
+        return a list of words
+    '''
+   #regex = re.compile('[^a-z]')
+   #return [regex.sub('',eachword.strip()) for eachword in text.lower().split(' ')]
+
+def build_search_index(docs):
+    '''
+        Process the docs step by step as given below
+    '''
+
+    # initialize a search index (an empty dictionary)
+
+    # iterate through all the docs
+    # keep track of doc_id which is the list index corresponding the document
+    # hint: use enumerate to obtain the list index in the for loop
+
+        # clean up doc and tokenize to words list
+
+        # add or update the words of the doc to the search index
+
+    # return search index
+    dict1 = {}
+    length = len(docs)
+    for index in range(length):
+        doc = word_list(docs[index])
+# helper function to print the search index
+# use this to verify how the search index looks
+    for i in doc:
+        word_count = doc.count[i]
+        if i in dict1:
+            
+        else:
+            dict1[i] = [(1,index)]
+    dict1 = del_stop(dict1)
+    return dict1
+
+
+
+
+def print_search_index(index):
+    '''
+        print the search index
+    '''
+    keys = sorted(index.keys())
+    for key in keys:
+        print(key, " - ", index[key])
+
+# main function that loads the docs from files
+def main():
+    '''
+        main function
+    '''
+    # empty document list
+    documents = []
+    # iterate for n times
+    lines = int(input())
+    # iterate through N times and add documents to the list
+    for i in range(lines):
+        documents.append(input())
+        i += 1
+
+    # call print to display the search index
+    print_search_index(build_search_index(documents))
+
+if __name__ == '__main__':
+    main()
