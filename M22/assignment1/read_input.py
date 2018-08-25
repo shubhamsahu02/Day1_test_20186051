439ceafc66c95387c9bf4773bb42b5d1dc472ea4
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-25 11:00:41 +0530

    " -> 2 of 2 passed. pylint: 6.00/10 "

--- M22/assignment1/read_input.py
+++ M22/assignment1/read_input.py
@@ -2,7 +2,7 @@
 Write a python program to read multiple lines of text input and store the input into a string.
 '''
 STR_ING = ""
- S_1= int(input())
+S_1= int(input())
 
 for i in range(S_1):
     STR_ING += input() +'\n'
