8ac4f35177bb0df86fc4b3acecd116f60c68185e
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-25 11:49:02 +0530

    " -> 3 of 3 passed. pylint: 4.55/10 "

--- M22/assignment2/clean_input.py
+++ M22/assignment2/clean_input.py
@@ -4,16 +4,17 @@ alphabets in both upper and lower case and numbers.
 '''
 import re
 def clean_string(string):
-    s = ""
+    STR_ING = ""
 
     for i in string:
-        s = re.sub('[^A-Za-z0-9]','', string.lower())
-        return s
+        STR_ING = re.sub('[^A-Za-z0-9]','', string.lower())
+        return STR_ING
 
    # regex = re.compile('[^A-Za-z0-9]')
    #return [regex.sub('', eachword.strip()) for eachword in string.lower().split(' ')]
 
 def main():
+    '''function'''
     string = input()
     print(clean_string(string))
 
